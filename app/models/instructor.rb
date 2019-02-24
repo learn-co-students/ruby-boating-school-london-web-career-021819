5b574c881dd9ae3fbb7c14060ec700db6e58104e
github username <github email address>
2019-02-24 19:08:49 +0000

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,34 @@
 class Instructor
+attr_reader :name
+@@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def pass_student(test_name, student)
+    tests = BoatingTest.all.select {|tests| test_name == tests.test_name}
+    tests.map {|test| test.test_status = "passed"}
+      if tests == []
+        BoatingTest.new(student, test_name, "passed", self)
+      else
+        tests
+      end
+  end
+
+  def fail_student(test_name, student_name)
+    test = BoatingTest.all.select {|test| test_name == test.test_name}
+    test.map {|test| test.test_status = "failed"}
+      if test == []
+        BoatingTest.new(student_name, test_name, "failed", self)
+      else
+        test
+      end
+  end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,28 @@
 class Student
+attr_reader :name
+@@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_student(name)
+    Student.all.select {|student| student.first_name == name}
+  end
+
+  def add_boating_test(test, status, instructor)
+    BoatingTest.new(self, test, status, instructor)
+  end
+
+  def grade_percentage
+    tests = BoatingTest.all.select {|test| test.student == self}.count.to_f
+    passed = BoatingTest.all.select {|test| test.student == self && test.test_status == "passed"}.count.to_f
+    ((passed/tests)*100).round(2)
+  end
 
 end
