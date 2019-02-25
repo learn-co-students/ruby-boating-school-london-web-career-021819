7288176c40c71460baacf73a08f919331753c3cb
github username <github email address>
2019-02-25 15:59:14 +0000

    Done.

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,15 @@
 class BoatingTest
-
+  attr_reader :student, :test, :instructor
+  attr_accessor :test_status
+  @@all = []
+  def initialize(student, test, test_status, instructor)
+    @student = student
+    @test = test
+    @test_status = test_status
+    @instructor = instructor
+    self.class.all << self
+  end
+  def self.all
+    @@all
+  end
 end
