d731d457390deff240a77d390d06436f1637d6fb
github username <vyskoutaris@hotmail.com>
2019-02-25 21:43:37 +0000

    Done

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,37 @@
+
 class Instructor
+
+attr_accessor :name
+
+@@instructors = []
+
+def initialize(name)
+  @name = name
+  @@instructors << self
+end
+
+def self.all
+  @@instructors
+end
+
+def pass_student(student, name)
+ found_test = BoatingTest.all.find{|test|test.name == name && test.student == student}
+  if found_test
+    found_test.status == "pass"
+  else
+    found_test = BoatingTest.new(student, name, self, "pass")
+  end
+  found_test
+end
+
+def fail_student(student, name)
+  found_test = BoatingTest.all.find{|test|test.name == name && test.student == student}
+  if found_test
+    found_test.status == "fail"
+  else
+    found_test = BoatingTest.new(student, name, self, "fail")
+  end
+  found_test
+end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,8 +1,29 @@
 require_relative '../config/environment.rb'
 
+
 def reload
   load 'config/environment.rb'
 end
+
+student1 = Student.new("Spongebob")
+student2 = Student.new("Patrick")
+student3 = Student.new("Octopus")
+student4 = Student.new("Rock")
+
+instructor1 = Instructor.new("Ms.Puff")
+instructor2 = Instructor.new("Mr.Krabs")
+instructor3 = Instructor.new("Mr. Shell")
+instructor4 = Instructor.new("Ms. Neon")
+
+test1 = BoatingTest.new(student1, "Don't Crash 101", instructor1)
+test2 = BoatingTest.new(student2, "Power Steering 201", instructor2)
+test3 = BoatingTest.new(student3, "Power Steering 202", "pass")
+test4 = BoatingTest.new(student4, "Power Steering 203", "fail")
+
+# no_crashing = spongebob.add_boating_test("Don't Crash 101", "pending", puff)
+# power_steering_failure = patrick.add_boating_test("Power Steering 202", "failed", puff)
+# power_steering_pass = patrick.add_boating_test("Power Steering 201", "passed", krabs)
+
 
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
@@ -10,4 +31,3 @@ end
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
-
