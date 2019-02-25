573adc7c55f8dd6ba4940dcce1c11233d6ff2bbd
Alex Bujenita <dekc89@gmail.com>
2019-02-25 12:02:53 +0000

    Boating DONE

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,18 @@
+# BoatingTest class
 class BoatingTest
+  @@all_boating_tests = []
 
+  def self.all
+    @@all_boating_tests
+  end
+  attr_accessor :boating_status
+  attr_reader :student, :boating_name, :instructor
+
+  def initialize(student, boating_name, boating_status, instructor)
+    @student = student
+    @boating_name = boating_name
+    @boating_status = boating_status
+    @instructor = instructor
+    @@all_boating_tests << self
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -8,6 +8,38 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
-binding.pry
-0 #leave this here to ensure binding.pry isn't the last line
+# Students
+alex = Student.new('Alex')
+jal = Student.new('Jalis')
+fede = Student.new('Fede')
+den = Student.new('Dennis')
+matt = Student.new('Matt')
+spongebob = Student.new('Spongebob')
+patrick = Student.new('Patrick')
 
+# Instructors
+puff = Instructor.new('Ms.Puff')
+krabs = Instructor.new('Mr.Krabs')
+enea = Instructor.new('Enea')
+
+# Boating tests created by Student class
+no_crashing = spongebob.add_boating_test("Don't Crash 101", 'pending', puff)
+power_steering_failure = patrick.add_boating_test('Power Steering 202', 'failed', puff)
+power_steering_pass = patrick.add_boating_test('Power Steering 201', 'pending', krabs)
+
+# Random create BoatingTest
+test01 = BoatingTest.new(alex, 'Dive', 'passed', krabs)
+test02 = BoatingTest.new(alex, 'Chem', 'failed', enea)
+test03 = BoatingTest.new(alex, 'Math', 'passed', krabs)
+test04 = BoatingTest.new(alex, 'Hunt', 'passed', krabs)
+test05 = BoatingTest.new(alex, 'Smith', 'failed', enea)
+test06 = BoatingTest.new(alex, 'Craft', 'pending', krabs)
+test07 = BoatingTest.new(alex, 'IAEUWY', 'failed', enea)
+# test08 = BoatingTest.new(alex, 'Smith', 'failed', enea)
+# test09 = BoatingTest.new(alex, 'Smith', 'failed', enea)
+# test10 = BoatingTest.new(alex, 'Smith', 'failed', enea)
+
+# For pass_student method
+pass_test_st = krabs.pass_student(patrick, 'Power Steering 201')
+binding.pry
+0 # leave this here to ensure binding.pry isn't the last line
