86c4f110260676a009afa02dab7b84c0675fe945
saqemlas <saqemlas@googlemail.com>
2019-02-25 17:13:15 +0000

    barely done

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,32 @@
 class Student
 
+  attr_accessor :first_name
+
+  @@all = []
+
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_boating_test(test_name, test_status, student, instructor)
+    BoatingTest.new(test_name, test_status, self, instructor)
+  end
+
+  def self.find_student
+    Student.all.select {|x| x.first_name == self}
+  end
+
+  def grade_percentage
+    student = BoatingTest.all.select {|x| x.student == self}
+    student_test_total = student.map {|x| x.test_status}.count
+    student_pass_total = student.map {|x| x.test_status}.count("passed")
+    average = student_pass_total.to_f/student_test_total.to_f
+    average * 100
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,10 +4,19 @@ def reload
   load 'config/environment.rb'
 end
 
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+student1 = Student.new("Stu")
+student2 = Student.new("Sofia")
+student3 = Student.new("Dan")
+
+instructor1 = Instructor.new("Alex")
+instructor2 = Instructor.new("Kian")
+instructor3 = Instructor.new("Sal")
+
+boatingtest1 = BoatingTest.new("testname1", "passed", student1, instructor1)
+boatingtest1 = BoatingTest.new("testname1", "passed", student1, instructor1)
+boatingtest1 = BoatingTest.new("testname1", "failed", student1, instructor1)
+boatingtest2 = BoatingTest.new("testname2", "teststatus2", student2, instructor2)
+boatingtest3 = BoatingTest.new("testname3", "teststatus3", student3, instructor3)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
-
